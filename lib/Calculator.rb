bb23a57a80aab4921005f1f413eab888c8c9fd51
Nossin A. Objio Saro <kosh61812@gmail.com>
2014-06-04 21:17:56 -0400

    implementando metodos

--- lib/Calculator.rb
+++ lib/Calculator.rb
@@ -1,10 +1,19 @@
 Class Calculator
+attr_reader :Num1,:Num2
+def initialize(num1,num2)
+@Num1 = num1
+@Num2 = num2
+end
 def sum()
+@Num1 + @Num2
 end
 def rest()
+@Num1 - @Num2
 end
 def mult()
+@Num1 * @Num2
 end
 def div()
+@Num1 / @Num2
 end
 end
